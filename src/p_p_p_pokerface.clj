a95f9140dff580931cb64b7168b566f0d611ca19
nick <nick@nick-Q524UQK>
2017-10-26 13:44:50 -0400

    0 tests failed.

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,64 @@
 (ns p-p-p-pokerface)
 
+(def ranks ["2" "3" "4" "5" "6" "7" "8" "9" "T" "J" "Q" "K" "A"])
+
 (defn rank [card]
-  nil)
+  (+ (.indexOf ranks (str (first card))) 2))
 
 (defn suit [card]
-  nil)
+  (str (second card)))
+
+(defn sorted [hand]
+  (sort (map rank hand)))
+
+(defn combo-freq [hand]
+  (let [ranks-map (map rank hand)
+        rank-freqs-map (frequencies ranks-map)
+        rank-freqs-vals (vals rank-freqs-map)]
+    (frequencies rank-freqs-vals)))
+
+(defn increments-by-1 [cards]
+  (let [sort (sorted cards)]
+    (apply = 1 (map - (rest sort) sort))))
+
+(defn combo-freq-size? [hand size]
+  (contains? (set (keys (combo-freq hand))) size))
 
 (defn pair? [hand]
-  nil)
+  (combo-freq-size? hand 2))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (combo-freq-size? hand 3))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (combo-freq-size? hand 4))
 
 (defn flush? [hand]
-  nil)
+  (= (count (frequencies (map suit hand))) 1))
 
 (defn full-house? [hand]
-  nil)
+  (and (pair? hand) (three-of-a-kind? hand)))
 
 (defn two-pairs? [hand]
-  nil)
+  (= (get (combo-freq hand) 2) 2))
 
 (defn straight? [hand]
-  nil)
+  (if (= (rank (last hand)) 14)
+    (and (= (rank (first hand)) 2) (increments-by-1 (take 4 hand)))
+    (increments-by-1 hand)))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
 
 (defn value [hand]
-  nil)
+  (cond
+    (straight-flush? hand)  8
+    (four-of-a-kind? hand)  7
+    (full-house? hand)      6
+    (flush? hand)           5
+    (straight? hand)        4
+    (three-of-a-kind? hand) 3
+    (two-pairs? hand)       2
+    (pair? hand)            1
+    :else                   0
+    ))
